341c82312438696145191f66ec34e6b4c5c77cfe
Jyoti <switjyoti26~@gmail.com>
2026-03-16 13:49:32 +0100

    Create logAfterDelay function with delay parameter

--- courses/frontend/advanced-javascript/week2/assignment/functions.js
+++ courses/frontend/advanced-javascript/week2/assignment/functions.js
@@ -2,3 +2,14 @@
 setTimeout(function () {
   console.log("Called after 2.5 seconds");
 }, 2500);
+
+//Function with delay and string
+function logAfterDelay(delay, stringToLog) {
+  setTimeout(function () {
+    console.log(stringToLog);
+  }, delay * 1000);
+}
+
+// Calling the function
+logAfterDelay(2, "Hello after 2 seconds");
+logAfterDelay(4, "Hello after 4 seconds");
