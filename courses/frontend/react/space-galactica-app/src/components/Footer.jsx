b1a44f5b019df6cb87b6d845fdb6ae31e5519bc6
Jyoti <switjyoti26~@gmail.com>
2026-04-20 12:17:33 +0200

    feat: add footer with pages and social media links

--- courses/frontend/react/space-galactica-app/src/components/Footer.jsx
+++ courses/frontend/react/space-galactica-app/src/components/Footer.jsx
@@ -1,51 +1,70 @@
 import { useLocation } from "react-router-dom";
-import styles from './Footer.module.css';
-
+import styles from "./Footer.module.css";
+import { Link } from "react-router-dom";
+import SocialMediaItem from "./SocialMediaItem";
 export const Footer = () => {
   const { pathname } = useLocation();
 
   return (
-    <footer className={pathname !== "/" ? styles.footer : styles.hidden}>
+    <footer className={styles.footer}>
       <div className={styles.footerDescription}>
         <h3>Galactica</h3>
-        <p>Explore the universe and beyond. Your journey to the stars starts here.</p>
+        <p>
+          Explore the universe and beyond. Your journey to the stars starts
+          here.
+        </p>
         <p>&copy; 2024 Galactica. All rights reserved.</p>
       </div>
       {/* 🧑🏽‍🚀 Task - Week 2 */}
       {/* Create a new list for the Pages. */}
       {/* We need to use the <Link /> component here. */}
-      {/* <div className={styles.pages}>
+      <div className={styles.pages}>
         <h3>Pages</h3>
         <ul>
-          <li> <Link/> </li>
-          ...
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/destination">Destinations</Link>
+          </li>
         </ul>
-      </div> */}
+      </div>
       {/* Docs for the Link: https://reactrouter.com/api/components/Link#link. */}
 
-      {/* 🧑🏽‍🚀 Task - Week 1 */}
-      {/* Add a new list item for LINKEDIN */}
       <div className={styles.footerLinks}>
         <h3>Follow us</h3>
         <ul className={styles.footerList}>
-          <li>
-            <a href="https://facebook.com">Facebook</a>
-          </li>
-          <li>
-            <a href="https://instagram.com">Instagram</a>
-          </li>
-          <li>
-            <a href="https://tiktok.com">Tiktok</a>
-          </li>
-          <li>
-            <a href="https://google.com">On the streets at night</a>
-          </li>
-          {/* 🧑🏽‍🚀 Task - Week 2 */}
-          {/* Create a <SocialMediaItem /> component and replace all of the list items! */}
-          {/* SocialMediaItem should accept the following props: url, title, icon. */}
-          {/* For the icons, you can download 1-2 social media icons for testing and put it in the /public/socialmedia/ folder. */}
+          <SocialMediaItem
+            url="https://facebook.com"
+            title="Facebook"
+            icon="/social media/facebook.png"
+          />
+
+          <SocialMediaItem
+            url="https://instagram.com"
+            title="Instagram"
+            icon="/social media/instagram.png"
+          />
+
+          <SocialMediaItem
+            url=""
+            title="Tiktok"
+            icon="/social media/tiktok.png"
+          />
+
+          <SocialMediaItem
+            url="https://LinkedIn.com"
+            title="LinkedIn"
+            icon="/social media/linkedin.png"
+          />
+
+          <SocialMediaItem
+            url="https://google.com"
+            title="On the streets at night"
+            icon="/social media/chrome.png"
+          />
         </ul>
       </div>
     </footer>
   );
-}
+};
