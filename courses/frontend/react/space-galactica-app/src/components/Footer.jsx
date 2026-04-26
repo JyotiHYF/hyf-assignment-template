37b4a1f30ee45013e2e3b83872e51f4dd27c9bac
Jyoti Pandey <switjyoti26@gmail.com>
2026-04-26 15:42:44 +0200

    Import Link alongside useLocation in Footer component

--- courses/frontend/react/space-galactica-app/src/components/Footer.jsx
+++ courses/frontend/react/space-galactica-app/src/components/Footer.jsx
@@ -1,7 +1,7 @@
-import { useLocation } from "react-router-dom";
+import { useLocation ,Link } from "react-router-dom";
 import styles from "./Footer.module.css";
 
-import { Link } from "react-router-dom";
+
 import SocialMediaItem from "./SocialMediaItem";
 
 export const Footer = () => {
