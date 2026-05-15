c3fea78b15262581d64c6f310353c85d1cf16fd1
Jyoti <switjyoti26~@gmail.com>
2026-05-15 09:08:13 +0200

    improve mobile responsiveness

--- courses/frontend/react/space-galactica-app/src/pages/HomePage/HomePage.jsx
+++ courses/frontend/react/space-galactica-app/src/pages/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
       <div className={styles.heroContent}>
         <p className={styles.heroH5}>SO, YOU WANT TO TRAVEL TO</p>
         <p className={styles.heroJumbo}>SPACE</p>
-        <p className={styles.heroContent}>
+        <p>
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
